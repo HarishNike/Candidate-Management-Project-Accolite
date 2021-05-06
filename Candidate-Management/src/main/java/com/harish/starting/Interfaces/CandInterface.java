e7150e7f974b66bf65e09d00cb6c3837be186568
HarishNike <49608687+HarishNike@users.noreply.github.com>
2021-05-06 16:13:39 +0530

    Delete Candidate-Management/src/main/java/com/harish/starting/Interfaces directory

--- Candidate-Management/src/main/java/com/harish/starting/Interfaces/CandInterface.java
+++
@@ -1,9 +0,0 @@
-package com.harish.starting.Interfaces;
-
-import java.util.List;
-
-import com.harish.starting.model.Candidate;
-
-public interface CandInterface {
-	public List<Candidate> getAllCandidates();
-}
